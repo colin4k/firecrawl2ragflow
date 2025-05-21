d5e455a368629cd2f89b7c3683ac20ee4de6a23b
colin4k <colin4k@gmail.com>
2025-05-21 11:08:52 +0800

    add title

--- crawl2rag.py
+++ crawl2rag.py
@@ -243,7 +243,7 @@ class Crawl2RAG:
                     logger.info(f'获取到HTML内容预览: {html_content_preview}')
                     
                     # 获取标题
-                    title = result.get('metadata', {}).get('title', '爬取页面')
+                    title = result['metadata']['title']
                     
                     # 保存为HTML文件
                     file_path = self._save_to_html(html_content, url, page_num, title)
